395d2a32472e72d5eeb043abadb9d1d1f87fe993
Leif Gensert <leif@leif.io>
2017-08-12 22:05:39 +0200

    refactor: remove unused function

--- lib/release_ping.ex
+++ lib/release_ping.ex
@@ -2,17 +2,4 @@ defmodule ReleasePing do
   @moduledoc """
   Documentation for ReleasePing.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ReleasePing.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
