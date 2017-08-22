2a9f288487c84e65a68cbb682f8b7747a7bc7513
Leif Gensert <leif@leif.io>
2017-08-22 08:55:01 +0200

    style: add typespecs to command and event modules

--- lib/release_ping/core/events/software_added.ex
+++ lib/release_ping/core/events/software_added.ex
@@ -1,5 +1,17 @@
 defmodule ReleasePing.Core.Events.SoftwareAdded do
+  alias ReleasePing.Core.Aggregates.Software
+
   @derive [Poison.Encoder]
+
+  @type t :: %__MODULE__{
+    uuid: String.t,
+    name: String.t,
+    website: String.t,
+    github: String.t,
+    licenses: [String.t],
+    release_retrieval: Software.release_retrieval,
+  }
+
   defstruct [
     uuid: nil,
     name: nil,
