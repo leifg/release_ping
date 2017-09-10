1d00f53a3673fb0979b9ccb0259900ec7ec776f3
Leif Gensert <leif@leif.io>
2017-09-10 17:37:53 +0200

    chore: rename db user
    release_ping is used in production and could help on locale development

--- config/dev.exs
+++ config/dev.exs
@@ -2,16 +2,16 @@ use Mix.Config
 
 config :eventstore, EventStore.Storage,
   serializer: Commanded.Serialization.JsonSerializer,
-  username: "postgres",
-  password: "postgres",
+  username: "release_ping",
+  password: "release_ping",
   database: "eventstore_dev",
   hostname: "localhost",
   pool_size: 10
 
 config :release_ping, ReleasePing.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "release_ping",
+  password: "release_ping",
   database: "release_ping_readstore_dev",
   hostname: "localhost",
   pool_size: 10
--- config/test.exs
+++ config/test.exs
@@ -2,16 +2,16 @@ use Mix.Config
 
 config :eventstore, EventStore.Storage,
   serializer: Commanded.Serialization.JsonSerializer,
-  username: "postgres",
-  password: "postgres",
+  username: "release_ping",
+  password: "release_ping",
   database: "eventstore_test",
   hostname: "localhost",
   pool_size: 1
 
 config :release_ping, ReleasePing.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "release_ping",
+  password: "release_ping",
   database: "release_ping_readstore_test",
   hostname: "localhost",
   pool_size: 1
