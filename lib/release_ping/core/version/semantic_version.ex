a86c49a59a86c3f517b72ae0d8c5ba6acb1b7e6d
Leif Gensert <leif@leif.io>
2017-10-12 08:56:04 +0200

    chore: remove unused version parsing

--- lib/release_ping/core/version/semantic_version.ex
+++ lib/release_ping/core/version/semantic_version.ex
@@ -1,6 +1,4 @@
 defmodule ReleasePing.Core.Version.SemanticVersion do
-  alias ReleasePing.Core.Version.{OtpVersion, PlainVersion}
-
   @type t :: %__MODULE__{
     major: non_neg_integer,
     minor: non_neg_integer,
@@ -9,12 +7,6 @@ defmodule ReleasePing.Core.Version.SemanticVersion do
   }
 
   defstruct [:major, :minor, :patch, :pre_release]
-
-  @type update_type :: :major | :minor | :patch
-
-  @callback parse(String.t) :: t
-  @callback parse(String.t, Regex.t) :: t
-  @callback name(String.t) :: String.t
 
   @spec parse(String.t, Regex.t) :: t
   def parse(version_string, version_scheme) do
@@ -49,40 +41,6 @@ defmodule ReleasePing.Core.Version.SemanticVersion do
   @spec default_version_scheme :: String.t
   def default_version_scheme do
     ~r/(?<major>\d+)\.(?<minor>\d+)(?:\.(?<patch>\d+))?(?:-(?<pre_release>.+))?/
-  end
-
-  @spec parse(String.t) :: t
-  def parse("v" <> rest_of_version) do
-    PlainVersion.parse(rest_of_version)
-  end
-
-  def parse("OTP-" <> rest_of_version) do
-    OtpVersion.parse(rest_of_version)
-  end
-
-  def parse("OTP_" <> rest_of_version) do
-    OtpVersion.parse(rest_of_version)
-  end
-
-  def parse(version) do
-    PlainVersion.parse(version)
-  end
-
-  @spec name(String.t) :: String.t
-  def name("v" <> rest_of_version) do
-    PlainVersion.name(rest_of_version)
-  end
-
-  def name("OTP-" <> rest_of_version) do
-    OtpVersion.name(rest_of_version)
-  end
-
-  def name("OTP_" <> rest_of_version) do
-    OtpVersion.name(rest_of_version)
-  end
-
-  def name(version) do
-    PlainVersion.name(version)
   end
 
   defp update_unless_blank(_k, v1, ""), do: v1
