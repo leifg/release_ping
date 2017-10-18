d355c26b6beef99c1ec11e4ad8c443665fa7b9a7
Leif Gensert <leif@leif.io>
2017-10-18 21:36:40 +0200

    fix: make adjusting of cursor more generic

--- lib/release_ping/scheduler.ex
+++ lib/release_ping/scheduler.ex
@@ -6,12 +6,18 @@ defmodule ReleasePing.Scheduler do
 
   use Quantum.Scheduler, otp_app: :release_ping
 
+  @to_adjust [
+    {"erlang", "otp"},
+  ]
+
   def poll_releases do
     Logger.info("Polling Releases")
 
     endpoint = GithubEndpoint.least_used()
 
-    adjust_cursor_for_erlang(endpoint)
+    Enum.each(@to_adjust, fn({repo_owner, repo_name}) ->
+      adjust_cursor(repo_owner, repo_name, endpoint)
+    end)
 
     ReleasePing.Core.github_release_pollers() |> Enum.each(fn(grp) ->
       [repo_owner, repo_name] = String.split(grp.repository, "/")
@@ -25,27 +31,30 @@ defmodule ReleasePing.Scheduler do
     end)
   end
 
-  defp adjust_cursor_for_erlang(github_endpoint) do
+  defp adjust_cursor(repo_owner, repo_name, github_endpoint) do
     import Ecto.Query, only: [from: 2]
+    github = "#{repo_owner}/#{repo_name}"
+
     query = from grp in "github_release_pollers",
-              where: grp.repository == "erlang/otp",
+              where: grp.repository == ^github,
               select: [grp.software_uuid, grp.last_cursor_tags]
 
-    [erlang_uuid, new_cursor] = ReleasePing.Repo.one(query)
+    case ReleasePing.Repo.one(query) do
+      nil -> :ok
+      [_software_uuid, nil] -> :ok
+      [software_uuid, new_cursor] ->
+        Logger.info("Adjust Cursor for #{repo_owner}/#{repo_name} to #{new_cursor}")
+        command = %ReleasePing.Incoming.Commands.AdjustCursor{
+          uuid: UUID.uuid4(),
+          github_uuid: github_endpoint.uuid,
+          software_uuid: UUID.binary_to_string!(software_uuid),
+          repo_owner: repo_owner,
+          repo_name: repo_name,
+          type: :tags,
+          cursor: new_cursor
+        }
 
-    if new_cursor != nil do
-      Logger.info("Adjust Cursor to #{new_cursor}")
-      command = %ReleasePing.Incoming.Commands.AdjustCursor{
-        uuid: UUID.uuid4(),
-        github_uuid: github_endpoint.uuid,
-        software_uuid: UUID.binary_to_string!(erlang_uuid),
-        repo_owner: "erlang",
-        repo_name: "otp",
-        type: :tags,
-        cursor: new_cursor
-      }
-
-      ReleasePing.Router.dispatch(command)
+        ReleasePing.Router.dispatch(command)
     end
   end
 end
