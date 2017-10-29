0ed4d95d1946f8adfb776f70edef6ee4da2e278b
Leif Gensert <leif@leif.io>
2017-10-29 19:25:19 +0100

    fix: always rest cursor to nil

--- lib/release_ping/scheduler.ex
+++ lib/release_ping/scheduler.ex
@@ -6,22 +6,23 @@ defmodule ReleasePing.Scheduler do
 
   use Quantum.Scheduler, otp_app: :release_ping
 
-  @to_adjust [
+  @to_reset MapSet.new([
     {"erlang", "otp"},
     {"fsharp", "fsharp"},
     {"golang", "go"},
+  ])
 
   def poll_releases do
     Logger.info("Polling Releases")
 
     endpoint = GithubEndpoint.least_used()
 
-    Enum.each(@to_adjust, fn({repo_owner, repo_name}) ->
-      adjust_cursor(repo_owner, repo_name, endpoint)
-    end)
-
     ReleasePing.Core.github_release_pollers() |> Enum.each(fn(grp) ->
       [repo_owner, repo_name] = String.split(grp.repository, "/")
+      if MapSet.member?(@to_reset, {repo_owner, repo_name}) do
+        adjust_cursor(repo_owner, repo_name, endpoint, grp.software_uuid)
+      end
+
       ReleasePing.Router.dispatch(%PollGithubReleases{
         uuid: UUID.uuid4(),
         github_uuid: endpoint.uuid,
@@ -32,30 +33,18 @@ defmodule ReleasePing.Scheduler do
     end)
   end
 
-  defp adjust_cursor(repo_owner, repo_name, github_endpoint) do
-    import Ecto.Query, only: [from: 2]
-    github = "#{repo_owner}/#{repo_name}"
+  defp adjust_cursor(repo_owner, repo_name, github_endpoint, software_uuid) do
+    Logger.info("Resetting Cursor for #{repo_owner}/#{repo_name}")
+    command = %ReleasePing.Incoming.Commands.AdjustCursor{
+      uuid: UUID.uuid4(),
+      github_uuid: github_endpoint.uuid,
+      software_uuid: software_uuid,
+      repo_owner: repo_owner,
+      repo_name: repo_name,
+      type: :tags,
+      cursor: nil
+    }
 
-    query = from grp in "github_release_pollers",
-              where: grp.repository == ^github,
-              select: [grp.software_uuid, grp.last_cursor_tags]
-
-    case ReleasePing.Repo.one(query) do
-      nil -> :ok
-      [_software_uuid, nil] -> :ok
-      [software_uuid, new_cursor] ->
-        Logger.info("Adjust Cursor for #{repo_owner}/#{repo_name} to #{new_cursor}")
-        command = %ReleasePing.Incoming.Commands.AdjustCursor{
-          uuid: UUID.uuid4(),
-          github_uuid: github_endpoint.uuid,
-          software_uuid: UUID.binary_to_string!(software_uuid),
-          repo_owner: repo_owner,
-          repo_name: repo_name,
-          type: :tags,
-          cursor: new_cursor
-        }
-
-        ReleasePing.Router.dispatch(command)
-    end
+    ReleasePing.Router.dispatch(command)
   end
 end
