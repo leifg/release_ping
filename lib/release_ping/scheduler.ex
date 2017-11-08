4c8d370a5947131dc9dd63ff6912d5cb7362b3ab
Leif Gensert <leif@leif.io>
2017-11-08 19:54:28 +0000

    fix: use github orderBy argument to get latest tags
    closes #244

--- lib/release_ping/scheduler.ex
+++ lib/release_ping/scheduler.ex
@@ -6,13 +6,6 @@ defmodule ReleasePing.Scheduler do
 
   use Quantum.Scheduler, otp_app: :release_ping
 
-  @to_reset MapSet.new([
-    {"erlang", "otp"},
-    {"fsharp", "fsharp"},
-    {"golang", "go"},
-    {"SWI-Prolog", "swipl-devel"},
-  ])
-
   def poll_releases do
     Logger.info("Polling Releases")
 
@@ -20,9 +13,6 @@ defmodule ReleasePing.Scheduler do
 
     ReleasePing.Core.github_release_pollers() |> Enum.each(fn(grp) ->
       [repo_owner, repo_name] = String.split(grp.repository, "/")
-      if MapSet.member?(@to_reset, {repo_owner, repo_name}) do
-        adjust_cursor(repo_owner, repo_name, endpoint, grp.software_uuid)
-      end
 
       ReleasePing.Router.dispatch(%PollGithubReleases{
         uuid: UUID.uuid4(),
@@ -32,20 +22,5 @@ defmodule ReleasePing.Scheduler do
         repo_name: repo_name,
       })
     end)
-  end
-
-  defp adjust_cursor(repo_owner, repo_name, github_endpoint, software_uuid) do
-    Logger.info("Resetting Cursor for #{repo_owner}/#{repo_name}")
-    command = %ReleasePing.Incoming.Commands.AdjustCursor{
-      uuid: UUID.uuid4(),
-      github_uuid: github_endpoint.uuid,
-      software_uuid: software_uuid,
-      repo_owner: repo_owner,
-      repo_name: repo_name,
-      type: :tags,
-      cursor: nil
-    }
-
-    ReleasePing.Router.dispatch(command)
   end
 end
