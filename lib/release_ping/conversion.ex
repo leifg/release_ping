a8847a5606ca844de6c0d71fdd7e693ce7e7467e
Leif Gensert <leif@leif.io>
2017-11-09 12:36:51 +0000

    fix: handle nil date

--- lib/release_ping/conversion.ex
+++ lib/release_ping/conversion.ex
@@ -1,4 +1,6 @@
 defmodule ReleasePing.Conversion do
+
+  def from_iso8601_to_naive_datetime(nil), do: nil
   def from_iso8601_to_naive_datetime(date_input) do
     NaiveDateTime.from_iso8601!(date_input)
   end
