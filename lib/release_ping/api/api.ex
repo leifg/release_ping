fb8b90cf10550f92322e94f7b17157df21d131ce
Leif Gensert <leif@leif.io>
2017-12-17 13:23:25 +0000

    fix: don't show software with empty version

--- lib/release_ping/api/api.ex
+++ lib/release_ping/api/api.ex
@@ -4,7 +4,13 @@ defmodule ReleasePing.Api do
 
   import Ecto.Query, only: [from: 2]
 
-  def all_software() do
-    Repo.all(from(s in Software, order_by: s.name))
+  def all_software do
+    Repo.all(
+      from(
+        s in Software,
+        order_by: s.name,
+        where: not(is_nil(s.latest_version_stable)) and not(is_nil(s.latest_version_unstable))
+      )
+    )
   end
 end
