04ae2af7fedbe79b18ce8531ad2cfe42cc095c8c
Leif Gensert <leif@leif.io>
2018-01-07 11:23:32 +0700

    chore: early exit when variables are not set correctly

--- deployment/determine_release.sh
+++ deployment/determine_release.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [[ -z "${GH_TOKEN}" ]]; then
+  echo "GH_TOKEN not set"
+  exit 1
+fi
+
 VERSION=$(semantic-release -dry -vf -slug leifg/release_ping -noci 2>&1 | grep "new version:" | egrep -o -e '[0-9]+\.[0-9]+\.[0-9]+' | head -n1)
 
 if [ -n "${VERSION}" ]; then
